963fa15ea59c0233053dfcec0801cdbf18859aab
josuegonzalez2 <josueg3028@gmail.com>
2022-10-25 23:06:40 -0700

    Landing Page - Added a dark background behind name and buttons for visibility. - Spaced Menu and order buttons apart
    Menu Page
    - Deleted unnecessary second menu.
    - Center aligned the headings.
    
    Order Page
    - Completed adding the rest of the item cards and modals.
    - Added details that pertained to each menu item.
    - center aligned headings.
    - Added dark background for visibility

--- MiMexicoWeb/Program.cs
+++ MiMexicoWeb/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{area=Admin}/{controller=Home}/{action=Landing}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Landing}/{id?}");
 
 app.Run();
