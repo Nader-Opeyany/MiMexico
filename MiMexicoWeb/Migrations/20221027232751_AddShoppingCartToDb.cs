472d47b2dddadd5e8ca0ef51ff145ecff445be5d
DannySteele67 <dales@DESKTOP-VV7CNFN>
2022-10-30 16:11:17 -0700

    BIG-115 (In Progress) Adding shopping cart model & database

---
+++ MiMexicoWeb/Migrations/20221027232751_AddShoppingCartToDb.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiMexicoWeb.Migrations
+{
+    public partial class AddShoppingCartToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCarts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    itemId = table.Column<int>(type: "int", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCarts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCarts_Items_itemId",
+                        column: x => x.itemId,
+                        principalTable: "Items",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCarts_itemId",
+                table: "ShoppingCarts",
+                column: "itemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingCarts");
+        }
+    }
+}
---
+++ MiMexicoWeb/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace MiMexicoWeb.Models
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int itemId { get; set; }
+        [ForeignKey("itemId")]
+        [ValidateNever]
+        public Item Item { get; set; }
+        public int quantity { get; set; }
+    }
+}
