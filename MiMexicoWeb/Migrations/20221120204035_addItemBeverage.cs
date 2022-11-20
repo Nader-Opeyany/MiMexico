c4f596fec149a6fac8cf17dfb7cfd7ffea11caaf
DannySteele67 <dales@DESKTOP-VV7CNFN>
2022-11-20 13:13:09 -0800

    BIG-115 (In Progress) Can now differenciate between drinks and food items when ordering. Added admin functionality to create drink items.

---
+++ MiMexicoWeb/Migrations/20221120204035_addItemBeverage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiMexicoWeb.Migrations
+{
+    public partial class addItemBeverage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Beverage",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Beverage",
+                table: "Items");
+        }
+    }
+}
